c056877339193232ab7fea93b7ae83ba8c67f0f1
unknown <Shreyas.Ambre@consultant.mannai.com.qa>
2025-10-24 11:29:11 +0530

    fixup(products): Categories API integrated in service and component

--- libs/products/src/lib/products/products.component.ts
+++ libs/products/src/lib/products/products.component.ts
@@ -1,11 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductsService } from '../data-access/services/products.service';
 
 @Component({
   selector: 'product-products',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+  ],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+
+  #productService = inject(ProductsService);
+
+  ngOnInit(): void {
+    this.getAllCategories();
+  }
+
+  getAllCategories(){
+    this.#productService.getCategories().subscribe(res => {
+      console.log("RES =>", res);
+
+    })
+  }
+}
